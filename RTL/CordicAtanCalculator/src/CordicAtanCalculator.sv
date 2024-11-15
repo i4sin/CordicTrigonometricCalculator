9055c5240389c13cb4be6d402922ebc6f791be9c
Yasin Mooasvi <smym4244@gmail.com>
2024-11-15 23:15:29 +0330

    improved in module and tb

--- RTL/CordicAtanCalculator/src/CordicAtanCalculator.sv
+++ RTL/CordicAtanCalculator/src/CordicAtanCalculator.sv
@@ -1,19 +1,65 @@
-module CordicAtanCalculator(
+module CordicAtanCalculator #(
+    parameter DATA_WIDTH,
+    parameter NUM_ITERATIONS
+) (
     input clk,
     input resetn,
-
-    input degree,
-    input valid,
-    output reg detected
+    input [DATA_WIDTH-1:0] x,
+    input [DATA_WIDTH-1:0] y,
+    output logic valid,
+    output logic [DATA_WIDTH-1:0] angle
 );
+    localparam real K_FACTOR = 0.607252935008881;
+
+    logic [DATA_WIDTH-1:0] atan_table [15:0];
+
+    logic [DATA_WIDTH-1:0] x_reg [NUM_ITERATIONS-1:0];
+    logic [DATA_WIDTH-1:0] y_reg [NUM_ITERATIONS-1:0];
+    logic [DATA_WIDTH-1:0] z_reg [NUM_ITERATIONS-1:0];
+
+    assign atan_table[0] = 45 * (1 << 16);
+    assign atan_table[1] = 26.565 * (1 << 16);
+    assign atan_table[2] = 14.036243467 * (1 << 16);
+    assign atan_table[3] = 7.125 * (1 << 16);
+    assign atan_table[4] = 3.576 * (1 << 16);
+    assign atan_table[5] = 1.790 * (1 << 16);
+    assign atan_table[6] = 0.895 * (1 << 16);
+    assign atan_table[7] = 0.447 * (1 << 16);
+    assign atan_table[8] = 0.224 * (1 << 16);
+    assign atan_table[9] = 0.112 * (1 << 16);
+    assign atan_table[10] = 0.057 * (1 << 16);
+    assign atan_table[11] = 0.028 * (1 << 16);
+    assign atan_table[12] = 0.014 * (1 << 16);
+    assign atan_table[13] = 0.007 * (1 << 16);
+    assign atan_table[14] = 0.004 * (1 << 16);
+    assign atan_table[15] = 0.002 * (1 << 16);
+
+    assign x_reg[0] = x;
+    assign y_reg[0] = y;
+    assign z_reg[0] = 0;
 
     always_ff @(posedge clk or negedge resetn) begin
         if (!resetn) begin
+            valid <= 0;
+            angle <= 0;
         end else begin
+            valid <= 0;
+            angle <= 0;
+            for (int i = 0; i < NUM_ITERATIONS; i++) begin
+                if (z_reg[i] < 0) begin
+                    x_reg[i + 1] <= x_reg[i] + (y_reg[i] >>> i);
+                    y_reg[i + 1] <= y_reg[i] - (x_reg[i] >>> i);
+                    z_reg[i + 1] <= z_reg[i] + atan_table[i];
+                end else begin
+                    x_reg[i + 1] <= x_reg[i] - (y_reg[i] >>> i);
+                    y_reg[i + 1] <= y_reg[i] + (x_reg[i] >>> i);
+                    z_reg[i + 1] <= z_reg[i] - atan_table[i];
+                end
+                if (i == NUM_ITERATIONS - 1) begin
+                    valid <= 1;
+                    angle <= z_reg[i] * K_FACTOR;
+                end
+            end
         end
-    end
-
-    always_comb begin
-
     end
 endmodule
--- RTL/CordicAtanCalculator/test/CordicAtanCalculator_tb.sv
+++ RTL/CordicAtanCalculator/test/CordicAtanCalculator_tb.sv
@@ -4,53 +4,93 @@
 import vunit_pkg::*;
 `include "vunit_defines.svh"
 
+module CordicAtanCalculator_tb #(
+
+) (
+    
+);
+endmodule
+
 module CordicAtanCalculator_tb();
+    parameter DATA_WIDTH = 0;
+    parameter NUM_ITERATIONS = 0;
+
     localparam TOTAL_WORDS_COUNT = 100000;
 
-    typedef logic logic_array[$];
-
-    logic_array seq_array;
-    logic_array expected_detected_array;
+    typedef logic [DATA_WIDTH-1:0] data_width_logic;
+    typedef data_width_logic logic_sequence[$];
+    typedef data_width_logic dual_logic_array[2];
+    typedef dual_logic_array dual_logic_sequence[$];
 
     bit clk = 0;
     bit resetn = 0;
     initial forever #(`PERIOD/2) clk = ~clk;
+
+    logic [DATA_WIDTH-1:0] input_x;
+    logic [DATA_WIDTH-1:0] input_y;
+    logic output_valid;
+    logic [DATA_WIDTH-1:0] output_angle;
+
+    dual_logic_sequence generated_sequence;
+    logic_sequence expected_angles;
     
-    CordicAtanCalculator sequenceDetector(
+    CordicAtanCalculator cordic_atan_calculator(
         .clk(clk),
         .resetn(resetn),
-        .seq(input_seq),
-        .valid(input_valid),
-        .detected(output_detected)
+        .x(input_x),
+        .y(input_y),
+        .valid(output_valid),
+        .angle(output_angle)
     );
 
-    function logic_array generate_random_seq();
-        logic_array random_seq;
-        $display("random_seq:\n %p", random_seq);
-        return random_seq;
+    function automatic dual_logic_array generate_random_x_y_array();
+        real x, y;
+        data_width_logic x_logic, y_logic;
+        dual_logic_array dual_logic_array;
+        x = $urandom_range(0, 1000) / 1000;
+        y = $sqrt(1 - x**2);
+        x_logic = x * (1 << 16);
+        y_logic = y * (1 << 16);
+        dual_logic_array[0] = x_logic;
+        dual_logic_array[1] = y_logic;
+        $display("dual_logic_array:\n %p", dual_logic_array);
+        return dual_logic_array;
     endfunction
 
-    function automatic logic_array expect_detected_output(logic_array seq_array);
-        logic_array expected_detected_array;
+    function automatic dual_logic_sequence generate_random_x_y_sequence();
+        dual_logic_sequence dual_logic_sequence;
+        for (int i = 0; i < TOTAL_WORDS_COUNT; i++) begin
+            dual_logic_sequence.push_back(generate_random_x_y_array());
+        end
+    endfunction
+
+    function automatic data_width_logic expect_angle(dual_logic_array array);//todo
+        dual_logic_array expected_detected_array;
         $display("expected_detected_array:\n %p", expected_detected_array);
         return expected_detected_array;
     endfunction
 
-    task drive_input(logic seq);
-        input_seq <= seq;
-        input_valid <= 1;
+    function automatic logic_sequence expect_angles(dual_logic_sequence seq); //todo
+        dual_logic_sequence expected_detected_sequence;
+        $display("expected_detected_sequence:\n %p", expected_detected_sequence);
+        return expected_detected_sequence;
+    endfunction
+
+    task drive_input(dual_logic_array array);
+        input_x <= array[0];
+        input_y <= array[1];
         @(posedge clk);
     endtask
 
-    task check_output(logic current_expected_output);        
-        assert (output_detected == current_expected_output) begin
-            $display("seq: %h, detected: %h, expected_output: %h",
-                        input_seq, output_detected, current_expected_output);
+    task check_output(data_width_logic current_expected_angle);        
+        assert (output_angle == current_expected_angle) begin
+            $display("x: %h, y: %h, angle: %h, expected_angle: %h",
+                        input_x, input_y, output_angle, current_expected_angle);
         end else $error("Operation wasn't done appropriately; output = %x, expected: %x", 
-                                                                output_detected, current_expected_output);
+                                                                output_angle, current_expected_angle);
     endtask
 
-    `TEST_SUITE begin
+    `TEST_SUITE_FROM_PARAMETER(runner_config) begin
         `TEST_CASE_SETUP begin
             resetn <= 0;
             repeat(6) @(posedge clk);
@@ -58,13 +98,13 @@ module CordicAtanCalculator_tb();
             repeat(6) @(posedge clk);
         end
 
-        `TEST_CASE("random_test_with_back_pressure") begin
-            seq_array = generate_random_seq();
-            expected_detected_array = expect_detected_output(seq_array);
+        `TEST_CASE("random_test") begin
+            generated_sequence = generate_random_x_y_sequence();
+            expected_angles = expect_angles(generated_sequence);
 
             for (int i = 0; i < TOTAL_WORDS_COUNT; i++) begin
-                drive_input(seq_array.pop_front());
-                check_output(expected_detected_array.pop_front());
+                drive_input(generated_sequence.pop_front());
+                check_output(expected_angles.pop_front());
             end
         end
 
